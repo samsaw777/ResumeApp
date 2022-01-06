8c2e3b67609fd0327c5d1ed0a83153237505a3fa
Sameep <sameepsawant10@gmail.com>
2022-01-06 18:36:18 +0530

    feat:educatin input added

--- components/education/EducationInput.tsx
+++ components/education/EducationInput.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 const EducationInput: React.FC = () => {
-  return <div>Education Input</div>;
+  return (
+    <div>
+      Education
+      <form>
+        <div>
+          <input
+            type="text"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 w-16"
+          />
+          <input
+            type="text"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 w-16"
+          />
+          <input
+            type="text"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export { EducationInput };

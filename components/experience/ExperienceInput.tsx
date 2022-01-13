a954ecdd840a05a71b964934c1ce23f70cc302ce
Sameep <sameepsawant10@gmail.com>
2022-01-13 22:59:25 +0530

    experience base folder created

---
+++ components/experience/ExperienceInput.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExperienceInput = () => {
+  return <div>This is Experience Input form</div>;
+};
+
+export default ExperienceInput;
---
+++ components/experience/ExperienceList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExperienceList = () => {
+  return <div>This is experiemce list.</div>;
+};
+
+export default ExperienceList;

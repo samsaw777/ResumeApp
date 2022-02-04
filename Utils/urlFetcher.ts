b5372eb1bf92b70d44af254d472b6b8bc3a15b4c
Sameep <sameepsawant10@gmail.com>
2022-02-04 13:16:49 +0530

    url changed

--- Utils/urlFetcher.ts
+++ Utils/urlFetcher.ts
@@ -2,5 +2,5 @@ export const urlFetcher = () => {
   //check if environment variable is development url localhost else producntion link
   return process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://nextjs-samsaw777.vercel.app";
+    : "https://resumeapp.vercel.app";
 };
